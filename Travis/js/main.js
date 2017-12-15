2a074d0552da790bf5a9df4d1cb223e6a9383bf2
Travis1282 <tmail20@gmail.com>
2017-12-15 11:16:25 -0600

    Commit 2 - Combine objects, arrays, and functions more than one level deep

--- Travis/js/main.js
+++ Travis/js/main.js
@@ -54,15 +54,54 @@
 			console.log("MyOtherFunctionIsWorking");
 		}
 		]
+		//actually would like to know how to call this function
+	//  myArrayThree[2];
+	// console.log(myArrayThree[2])
 
-	 myArrayThree[2];
-	console.log(myArrayThree[2])
 // Create an object that has a property that is an array. Loop over the array and log each individual element.
+
 // Create an array that has an array as one of its elements. Loop over the second array and log each individual element.
 // Bonus: make each element of the outer array an array as well. Using two for loops, loop over each array in the outer array and log those elements.
 
 
+// Create a function that returns an object. Log a property of that object (hint: call the function and then call a property on the return value).
+	
+	myFunct=()=>{
+		return {myObj:{ 1:"one",2:"two",3:"three"}
+			}
+	}
+	console.log(myFunct().myObj[1])
 
+// Create a function that returns an array. Log an element of the array.
+
+	myFunctTwo=()=>{
+		return {myArray:["one","two","three"]
+			}
+	}
+	console.log(myFunctTwo().myArray[0])
+
+// Create a function that returns an object that has an array. Log one of the elements of that array.
+	
+	myFunctThree=()=>{
+		return {myObj: {myObjArray:["one","two","three"]}
+			}
+	}
+	console.log(myFunctThree().myObj.myObjArray[0])
+
+
+// Create a function that returns an object that has an object. Log one of the properties of the inner object.
+// Create a function that returns an object that has a method. Call that inner function (method).
+// Create a function that returns a function. Call that inner function
+// Create an object that has a method that returns an object. Log a property of the inner object.
+// Create an object that has a method that returns an object that has an array. Log an element of that array.
+// Create an object that has a method that returns an object that has an object. Log a property of the inner object.
+// Create an object that has a method that returns an object that has another method. Call the inner method.
+// Create an object that has a method that returns a function. Call that function.
+// Create an array that has a function that returns an object. Log a property of the object.
+// Create an array that has a function that returns an object that has an array. Log an element of the inner array.
+// Create an array that has a function that returns an object that has an object. Log a property of the inner object.
+// Create an array that has a function that returns an object that has a method. Call that method.
+// Create an array that has a function that returns a function. Call the inner function.
 
 
 
