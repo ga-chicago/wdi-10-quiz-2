1c7156ff4b95185f98d1728f417beae49c09d1d4
Sierra Moore <sierra.v.moore@gmail.com>
2017-12-15 11:06:46 -0600

    Commit 3 - Create a callback

---
+++ app.js
@@ -0,0 +1,80 @@
+// //1 object with array property
+// let myObject = {
+//     thisKey: [1,2,3,4]
+// };
+// console.log(myObject.thisKey[0]);
+//
+// //2 object with inner object
+// let obj = {
+//     good: {
+//         bad: "great"
+//     }
+// };
+// console.log(obj.good.bad);
+//
+// // 3
+// let otherObj = {
+//     doSomething: function (){
+//         console.log("this is a function");
+//     }
+// };
+// otherObj.doSomething();
+//
+// //4 array with object
+// let myArray = [
+//     {cool: "stuff"}
+// ];
+// console.log(myArray[0]);
+//
+// //5 array with array
+// let otherArr = [
+//     [1,2,3]
+// ];
+// console.log(otherArr[0][1]);
+//
+// // 6
+// let arr = [1,2, function(){console.log("this is an array function")}];
+// arr[2]();
+//
+// //7
+// let objArr = {
+//     [1,2,3,4]
+// };
+// for(i=0; i < objArr.length; i++){
+//     console.log(objArr[0])
+// }
+//
+// //8 moving on
+// let array1 = [["a", "b", "c"]];
+// for(i=0; i < array1.length; i++){}
+
+/* create callback */
+//1
+let abc = function (){
+    console.log("hi");
+    param();
+};
+let zyx = function (param){
+    console.log("bye");
+};
+abc(zyx());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
