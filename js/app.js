ab39a21c400f7470248ff1ccb2df274907ac562c
Charles <because@charlesis.me>
2017-12-15 10:41:54 -0600

    Answer for 3

--- js/app.js
+++ js/app.js
@@ -2,9 +2,14 @@ let answer = {
     moreAnswers: [1,2,3,4,5],
     theresMore: {
         but: "wait",
+    },
+    manKind: function () {
+        console.log("Nay, I am the world")
     }
 }
 
 //console.log(answer.moreAnswers[2])
 
-console.log(answer.theresMore.but);
+//console.log(answer.theresMore.but);
+
+answer.manKind()
