0aefaebd9a97b12270625bcaa758fb88beec4da0
Anthony <anthonyjlower@gmail.com>
2017-12-15 10:44:43 -0600

    Commit 1 - Combine objects, arrays, and functions

---
+++ js/app.js
@@ -0,0 +1,26 @@
+// COMBINE OBJECTS ARRAYS AND FUNCTIONS
+
+// 1.
+
+const obj1 = {
+	array1: [1,2,3,4],
+}
+console.log(obj1.array1[2]);
+
+// 3.
+
+const obj3= {
+	someMethod() {
+		return "This is the method from question 3"
+	}
+}
+console.log(obj3.someMethod())
+
+// 4. 
+
+const someArray = [
+	someObj = {
+		name: "Anthony"
+	}
+]
+console.log(someArray[0].name)
