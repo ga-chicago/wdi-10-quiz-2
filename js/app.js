2693b0984082b4b33dfd2d0cdd86d9fb2debe70c
Ryne Laster <ryne.laster@gmail.com>
2017-12-15 11:06:42 -0600

    Commit 1- objects, arrays, and functions

---
+++ js/app.js
@@ -0,0 +1,31 @@
+console.log('yo')
+// combine objects, arrays, and functions
+const curbArray = [
+	characters: {
+		name1: 'jeff',
+		name2: 'larry',
+		name3: 'susie',
+		name4: 'cheryl',
+		name5: 'funkhouser'
+	}
+]
+console.log(curbArray.name5)
+
+
+
+
+let curbObj = {
+	funny: true,
+	characters: ['larry', 'jeff', 'cheryl', 'susie', 'funkhouser']
+};
+console.log(curbObj.characters[4])
+
+let curb2 = [
+	characters: ['larry', 'jeff', 'cheryl', 'susie', 'funkhouser']
+	seasons: ['season 1', 'season 2', 'season 3', 'season 4', 'season 5', 'season 6', 'season 7', 'season 8', 'season 9'  ]
+
+]
+console.log(curb2[1][5])
+
+// combine ojects, arrays, and functions
+
