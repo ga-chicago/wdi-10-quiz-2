61594dc05bc2936c902360bab15151c4bab74f04
Charles <because@charlesis.me>
2017-12-15 10:40:03 -0600

    Answer for 2

--- js/app.js
+++ js/app.js
@@ -1,5 +1,10 @@
 let answer = {
-    moreAnswers: [1,2,3,4,5]
+    moreAnswers: [1,2,3,4,5],
+    theresMore: {
+        but: "wait",
+    }
 }
 
-console.log(answer.moreAnswers[2])
+//console.log(answer.moreAnswers[2])
+
+console.log(answer.theresMore.but);
