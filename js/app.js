602192a80e806fd692ec76ea5433f947632c6d4b
Kait Mikitin <kait.mikitin@gmail.com>
2017-12-15 11:11:54 -0600

    Commit 3 - Create a Callback

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 console.log('Allons-y!')
 
+// 1. Combine objects, arrays, and functions
 // Create an object that has a property that is an array. Log one of the elements of that array.
 let theDoctor = {
 	regeneration: 'Ten',
@@ -34,3 +35,46 @@ let serenity = {
 	}
 
 }
+
+serenity.newPassengers(1, 'River')
+
+
+// 2. Combine objects, arrays, and functions
+// Create a function that returns an object that has an array. Log one of the elements of that array.
+
+let thisFunction = () => {
+
+}
+
+// Create an object that has a method that returns an object. Log a property of the inner object.
+
+let thisObj = {
+
+}
+
+// Create an array that has a function that returns an object that has an array. Log an element of the inner array.
+
+let thisArray = []
+
+
+// 3. Create a callback
+// Define two functions and set them to variables
+// The second function takes a parameter
+// Call the second function, passing in the variable that references the first function as the parameter
+// In the definition of the second function, invoke (call) the parameter that is being passed into it. Remember, this parameter is a function
+
+let firstFunction = () => {
+	console.log('calling the first function');
+}
+
+let secondFunction = (callFunction) => {
+	callFunction();
+	console.log('running the second function');
+}
+
+secondFunction(firstFunction)
+
+
+
+
+
