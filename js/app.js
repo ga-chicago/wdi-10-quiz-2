8ed25eef9891855a8a641165a3c9f818c45f83b0
Michael Weber <webermn15@gmail.com>
2017-12-15 10:43:15 -0600

    Commit 2 - Combine objects, arrays, and functions more than one level deep

--- js/app.js
+++ js/app.js
@@ -19,8 +19,70 @@ const funObj = {
 // console.log(funObj.sayHello());
 
 
+//Create an array that has an object in it. Log one of the properties of that object.
+
 const anArr = [{
 	objProp: 'Michael'
 }];
 
-console.log(anArr[0].objProp);
+// console.log(anArr[0].objProp);
+
+
+
+//Create a function that returns an array. Log an element of the array.
+
+let myFunc = () => {
+	return [1,2,3];
+};
+
+// console.log(myFunc()[1]);
+
+
+// Create a function that returns an object that has an array. Log one of the elements of that array.
+
+const anotherFunc = () => {
+	return {
+		arrProp: [1,2,3,4]
+	}
+};
+
+// console.log(anotherFunc().arrProp[3]);
+
+
+//Create a function that returns a function. Call that inner function
+
+const inceptionFunction = () => {
+	return () => {
+		console.log('BRRREEEEUUUUAHHAHHHAH');
+	}
+}
+
+inceptionFunction()();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
